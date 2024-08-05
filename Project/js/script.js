fa2008002f75f150f27c1ab145ce0afe4a5f32f5
yurika-s <200558167@student.georgianc.on.ca>
2024-08-05 14:44:11 -0400

    Add home page contents

---
+++ Project/js/script.js
@@ -0,0 +1,15 @@
+function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeaderImg = document.querySelector('header .logo img');
+
+    if (window.scrollY > 100) {
+        selectBody.classList.add('scrolled');
+        selectHeaderImg.src = './image/logo.png';
+    } else {
+        selectBody.classList.remove('scrolled');
+        selectHeaderImg.src = './image/logo-yellow.png';
+    }
+}
+
+
+window.addEventListener("scroll", toggleScrolled);

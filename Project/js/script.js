4143214e41a77ca52bbd22d4a54adf1e0cb212c5
yurika-s <200558167@student.georgianc.on.ca>
2024-08-06 13:01:05 -0400

    Add products pages

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,15 +1,26 @@
-function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeaderImg = document.querySelector('header .logo img');
+// constants for selectors
+const LOGO_IMAGE = 'logo.png';
+const LOGO_IMAGE_YELLOW = 'logo-yellow.png';
+const IMAGE_DIRECTORY = 'image/';
 
+function toggleScrolled() {
     if (window.scrollY > 50) {
-        selectBody.classList.add('scrolled');
-        selectHeaderImg.src = './image/logo.png';
+        $('body').addClass('scrolled');
+        // set default image path
+        let imagePath = './' + IMAGE_DIRECTORY + LOGO_IMAGE;
+        if ($('body').attr('id') === 'products') {
+            // change the path when the page is products
+            imagePath = '../' + IMAGE_DIRECTORY + LOGO_IMAGE;
+        }
+        $('header .logo img').attr('src', imagePath)
     } else {
-        selectBody.classList.remove('scrolled');
-        selectHeaderImg.src = './image/logo-yellow.png';
+        $('body').removeClass('scrolled');
+        let imagePath = './' + IMAGE_DIRECTORY + LOGO_IMAGE_YELLOW;
+        if ($('body').attr('id') === 'products') {
+            imagePath = '../' + IMAGE_DIRECTORY + LOGO_IMAGE_YELLOW;
+        }
+        $('header .logo img').attr('src', imagePath)
     }
 }
-
 
 window.addEventListener("scroll", toggleScrolled);
